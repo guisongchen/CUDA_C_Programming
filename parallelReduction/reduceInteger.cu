a036b12cbc8c3cefc87e031a3e228a8552afdafd
ccc <guisongchen@163.com>
2022-02-01 15:56:25 +0800

    unroll warp done

--- parallelReduction/reduceInteger.cu
+++ parallelReduction/reduceInteger.cu
@@ -187,6 +187,62 @@ __global__ void reduceUnrolling4 (int *g_idata, int *g_odata, unsigned int n)
     if (tid == 0) g_odata[blockIdx.x] = idata[0];
 }
 
+// Unrolling loop Implementation with less divergence
+// different from reduceUnrolling4:
+// 1. stop unroll warps when stride < 32, a.k.a. stride=32, 16, 8, 4, 2, 1 
+//    because left threads are not enough for a warp
+//    if keep unroll, the left threads must be padded into a warp(32 threads)
+//    so some threads in the last warp will be halting, warp divergence happens
+// 2. instead of putting the left threads in a warp, unroll them explicitly
+//    usin stride 32, 16, 8, 4, 2, 1
+__global__ void reduceUnrollWarps4 (int *g_idata, int *g_odata, unsigned int n)
+{
+    // set thread ID
+    unsigned int tid = threadIdx.x;
+
+    // unrolling neighbore
+    unsigned int idx = blockIdx.x * blockDim.x * 4 + threadIdx.x;
+
+    // unrolling current and neighbore block(+blockDim.x)
+    // at g_idata !!!
+    if (idx + blockDim.x*3 < n) {
+        int a = g_idata[idx];
+        int b = g_idata[idx+blockDim.x];
+        int c = g_idata[idx+blockDim.x*2];
+        int d = g_idata[idx+blockDim.x*3];
+        g_idata[idx] = a+b+c+d;
+    }
+    __syncthreads();
+
+    // convert global data pointer to the local pointer of this block
+    int *idata = g_idata + blockIdx.x * blockDim.x * 4;
+
+    // in-place reduction in global memory
+    // notice: stride > 32 !!!
+    for (int stride = blockDim.x/2; stride > 32; stride >>= 1)
+    {
+        if (tid < stride) {
+            idata[tid] += idata[tid + stride];
+        }
+        // synchronize within threadblock
+        __syncthreads();
+    }
+
+    // unrolling warp
+    if (tid < 32) {
+        volatile int *vmem = idata;
+        vmem[tid] += vmem[tid+32];
+        vmem[tid] += vmem[tid+16];
+        vmem[tid] += vmem[tid+8];
+        vmem[tid] += vmem[tid+4];
+        vmem[tid] += vmem[tid+2];
+        vmem[tid] += vmem[tid+1];
+    }
+
+    // write result for this block to global mem
+    if (tid == 0) g_odata[blockIdx.x] = idata[0];
+}
+
 int main(int argc, char **argv)
 {
     // set up device
@@ -333,6 +389,22 @@ int main(int argc, char **argv)
     for (int i = 0; i < grid.x / 4; i++) gpu_sum += h_odata[i];
 
     printf("gpu Unrolling4  elapsed %f sec gpu_sum: %d <<<grid %d block "
+           "%d>>>\n", iElaps, gpu_sum, grid.x / 4, block.x);
+
+    // kernel 6: reduceUnrollWarps4
+    CHECK(cudaMemcpy(d_idata, h_idata, bytes, cudaMemcpyHostToDevice));
+    CHECK(cudaDeviceSynchronize());
+    iStart = seconds();
+    reduceUnrollWarps4<<<grid.x / 4, block>>>(d_idata, d_odata, size);
+    CHECK(cudaDeviceSynchronize());
+    iElaps = seconds() - iStart;
+    CHECK(cudaMemcpy(h_odata, d_odata, grid.x / 4 * sizeof(int),
+                     cudaMemcpyDeviceToHost));
+    gpu_sum = 0;
+
+    for (int i = 0; i < grid.x / 4; i++) gpu_sum += h_odata[i];
+
+    printf("gpu UnrollWarp4 elapsed %f sec gpu_sum: %d <<<grid %d block "
            "%d>>>\n", iElaps, gpu_sum, grid.x / 4, block.x);
 
     // free host memory
