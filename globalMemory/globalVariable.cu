71d4d1d622948d5d2d9b0f09311f28dc46bc115b
ccc <guisongchen@163.com>
2022-02-02 18:31:51 +0800

    done exercises 1

--- globalMemory/globalVariable.cu
+++ globalMemory/globalVariable.cu
@@ -1,22 +1,26 @@
 #include <cuda_runtime.h>
 #include <stdio.h>
 
-__device__ float devData;
+__device__ float devData[5];
 
 __global__ void checkGlobalVal() {
-    printf("device: %f\n", devData);
-    devData += 2.0f;
+    devData[threadIdx.x] *= threadIdx.x;
+    printf("device threadIdx:%d, val:%f\n", threadIdx.x, devData[threadIdx.x]);
 }
 
 int main(int argc, char **argv) {
-    float val = 3.14f;
-    cudaMemcpyToSymbol(devData, &val, sizeof(float));
-    printf("host: %f\n", val);
+    float val[5];
+    for (int i = 0; i < 5; i++)
+        val[i] = 3.14f;
+    cudaMemcpyToSymbol(devData, val, 5*sizeof(float));
+    for (int i = 0; i < 5; i++)
+        printf("host idx:%d, val:%f\n", i, val[i]);
 
-    checkGlobalVal<<<1,1>>>();
+    checkGlobalVal<<<1,5>>>();
 
-    cudaMemcpyFromSymbol(&val, devData, sizeof(float));
-    printf("host, changed value: %f\n", val);
+    cudaMemcpyFromSymbol(val, devData, 5*sizeof(float));
+    for (int i = 0; i < 5; i++)
+        printf("host idx:%d, changed val:%f\n", i, val[i]);
 
     cudaDeviceReset();
 
